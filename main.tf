a6f4d1a86592feacd5f72ccf5675543fd5c8415a
tomon9086 <tomon9086@gmail.com>
2023-11-30 17:33:11 +0900

    remove useless resource def.

--- main.tf
+++ main.tf
@@ -32,11 +32,6 @@ resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
 
-resource "google_service_account" "default" {
-  account_id   = "test-gcf-sa"
-  display_name = "Test Service Account"
-}
-
 resource "google_pubsub_topic" "default" {
   name = "functions2-topic"
 }
@@ -130,7 +125,6 @@ resource "google_cloudfunctions2_function" "test-pubsub" {
     }
     ingress_settings               = "ALLOW_INTERNAL_ONLY"
     all_traffic_on_latest_revision = true
-    service_account_email          = google_service_account.default.email
   }
 
   event_trigger {
